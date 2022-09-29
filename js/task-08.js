3886344a51da5dd37030cefbadb4e4579e99b889
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-09-29 10:39:40 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault(); //*не будет перезагрузки страницы при отправкe формы
+
+    const formElements = event.currentTarget.elements; //*путь к данным формы
+
+    const mail = formElements.email.value;  //* полный путь к значению почты
+    const password = formElements.password.value; //* полный путь к значению пароля
+
+    const formData = {  //* создал объект в который положил данные с формы
+        mail,
+        password,
+    };
+
+    if (mail === "" || password === "") {
+    return alert("Please fill in all the fields!");
+    }
+
+    console.log(formData); //*например {mail: 'asd@qwe.poi', password: '123eqw'}
+
+    event.currentTarget.reset();
+}
+
 
