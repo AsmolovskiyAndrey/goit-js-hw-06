11f43858c012b8a5cf64f2716a3ff70e3e55d414
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-09-29 11:06:11 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+function changeBackgroundColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorRef.textContent = bodyRef.style.backgroundColor;
+}
+
+const bodyRef = document.querySelector('body');
+const btnRef = document.querySelector('.change-color');
+const colorRef = document.querySelector('.color');
+
+btnRef.addEventListener('click', changeBackgroundColor);
+
+
